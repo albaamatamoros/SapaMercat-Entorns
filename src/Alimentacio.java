ad02c9b48ad3bf0e22d8b7779d7031ea2eac9861
albaamatamoros <albamamo07@gmail.com>
2024-05-03 13:29:03 +0200

    Acabar de rellenar cosas de Alimentacio + Producte y modificar cosas exsistentes

--- src/Alimentacio.java
+++ src/Alimentacio.java
@@ -1,9 +1,40 @@
+import java.util.Date;
+
 public class Alimentacio extends Producte{
     String dataCaducitat;
 
     //CONSTRUCTOR Alimentacio
     public Alimentacio(float preu, String nom, String codiDeBarres, String dataCaducitat) {
         super(preu, nom, codiDeBarres);
+        setDataCaducitat(dataCaducitat);
+    }
+
+    public String getDataCaducitat() {
+        return dataCaducitat;
+    }
+
+    public void setDataCaducitat(String dataCaducitat) {
         this.dataCaducitat = dataCaducitat;
+    }
+
+    //Calculem el preu segons la data de caducitat.
+    public float getPreu() {
+        float preu = super.getPreu();
+        //Creem un objecte Date que guarda la data i hora actual.
+        Date data = new Date(System.currentTimeMillis());
+        Date dataCad = new Date(dataCaducitat);
+
+        //Fem un càlcul però obtenir la diferència de dies entre dues dates.
+        int dias = (int) ((dataCad.getTime() - data.getTime()));
+
+        //Retornem el nou preu del producte basant-nos en els dies que li queden per caducar.
+        return (float) (preu - preu*(1 /(dias+1)) - (preu * 0.1));
+    }
+
+    @Override
+    public String toString() {
+        if (getPreu() <= 0){ return String.format("%10s 0€", nom); }
+        else { return String.format("%10s %10.2f", nom, getPreu()); }
+
     }
 }
--- src/Producte.java
+++ src/Producte.java
@@ -11,4 +11,24 @@ public abstract class Producte {
         this.codiDeBarres = codiDeBarres;
     }
 
+    public float getPreu() {
+        return preu;
+    }
+
+    public void setPreu(float preu) {
+        this.preu = preu;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCodiBarres() {
+        return codiDeBarres;
+    }
+
 }

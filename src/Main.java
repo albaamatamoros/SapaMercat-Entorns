876dd866ef6e989b610fd6f420c3f4e36af84347
albaamatamoros <albamamo07@gmail.com>
2024-05-03 18:43:31 +0200

    Menú productes + metodes afegir producte

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scan = new Scanner(System.in);
+    //VARIABLES GLOBALS
     static String opcio;
-
+    private static final int MAX_CARRO = 100;
+    private static final int MAX_LLARG = 15;
     public static void main(String[] args) {
         //Cridem el mètode menuInici.
         menuInici();
@@ -42,4 +44,43 @@ public class Main {
             }
         } while (!(opcio.equals("0")));
     }
+
+    public static void introduirProducte(){
+        //MENÚ PRODUCTE:
+        do {
+            System.out.println("BENVINGUT AL SAPAMERCAT");
+            System.out.println("---------------");
+            System.out.println("-- PRODUCTE ---");
+            System.out.println("---------------");
+            System.out.println("1) Alimentació");
+            System.out.println("2) Tèxtil");
+            System.out.println("3) Electrònica");
+            System.out.println("0) Tornar");
+            opcio = scan.nextLine();
+            switch (opcio) {
+                case "1":
+                    afegirProducteAlimentacio();
+                    break;
+                case "2":
+                    afegirProducteTextil();
+                    break;
+                case "3":
+                    afegirProducteElectronica();
+                    break;
+                case "0":
+                    menuInici();
+                    break;
+                default:
+                    System.out.println("-------------------------------");
+                    System.out.println("ATENCIÓ! Ha de ser entre 0 i 3");
+                    System.out.println("-------------------------------");
+            }
+        } while (!(opcio.equals("0")));
+    }
+
+    //AFEGIR PRODUCTES:
+    //Mètode per afegir un producte alimentacio.
+    private static void afegirProducteAlimentacio(){}
+    private static void afegirProducteTextil(){}
+    private static void afegirProducteElectronica(){}
 }
